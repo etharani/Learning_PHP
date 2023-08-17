605def0684e1642eba07f0bdd1779ed82dbd6fa2
etharani <tharani2297@gmail.com>
2023-08-17 16:02:18 +0530

    First upload in PHP

---
+++ d1_firstday.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>start work html</title>
+</head>
+<body>
+<h1>Welcome html</h1>
+<?php
+echo "<h1>welcome home page</h1>";
+echo"<h1>welcome home page</h1>","welcome home first page","HI";
+print"<h1>welcome hoe page - print</h1>";
+
+//print"<h1>welcome home page</h1>","welcome home first page","HI";
+ECHO "<h1>welcome home page</h1>";
+Echo "<h1>welcome home page</h1>";
+
+$name=10;
+echo $name;
+
+echo"<br>";
+echo"$name";
+
+/**echo"<br>";
+echo'$name'; **/
+
+?>
+</body>
+</html> 
---
+++ d2_second day.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>start work html</title>
+</head>
+<body>
+<h1>Welcome html</h1>
+<?php
+$name="Tharani";
+echo "my name is ".$name;
+echo"<br>";
+echo "my name is ",$name;
+echo"<br>";
+echo "my name is $name";
+echo"<br>";
+echo 'my name is ',$name;
+echo"<br>";
+echo 'my name is '.$name;
+echo"<br>";
+echo "my name is {$name}";
+echo"<br>";
+echo "my name is {$name}Easwaranathan";
+echo"<br>";
+echo "my name is".$name."Easwaranathan";
+echo"<br>";
+echo "my name is",$name,"Easwaranathan"; 
+echo"<br>";
+echo 'my name is'.$name.'Easwaranathan';
+echo "<br>";
+var_dump($name);
+
+define("NAME","Kithusha");
+echo"<br>";
+echo NAME;
+
+// Home work
+$first_name="Tharani";
+$last_name="Easwaranathan";
+$age=25;
+$gender="female";
+$telephone_number="0774411148";
+echo"<br>";
+echo"<br>";
+echo"First name   :{$first_name}";
+echo"<br>";
+echo"Last name    :{$last_name}";
+echo"<br>";
+echo"Age          :{$age}";
+echo"<br>";
+echo"Gender       :{$gender}";
+echo"<br>";
+echo"Tele number  :{$telephone_number}";
+
+
+
+
+?>
+</body>
+</html>
